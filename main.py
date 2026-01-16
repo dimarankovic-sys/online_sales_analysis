1efe71e3d745547c1f06de3fcc6da8be3b4f8182
dimarankovic-sys <dima.rankovic@gmail.com>
2026-01-16 20:19:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from product import Product
 from product_manager import ProductManager
-from cart import Cart
-import random
 
 manager = ProductManager()
 
-manager.add_product(Product("Laptop", 1200, 3))
-manager.add_product(Product("Telefon", 800, 4))
-manager.add_product(Product("Slusalice", 150, 6))
+manager.add_product(Product("Laptop Pro", 1500, 2))
+manager.add_product(Product("Telefon X", 900, 5))
 
-cart = Cart()
-
-for product in random.sample(manager.products, 3):
-    cart.add_to_cart(product)
-
-cart.display_cart()
-print("Ukupno za naplatu:", cart.total_price())
